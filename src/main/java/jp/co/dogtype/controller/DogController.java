e740cb80a7e817bccafccb2a0eca8b075794c52b
yo-chan <whitesnow.wave@gmail.com>
2022-04-21 15:51:19 +0900

    画面に一覧を表示

---
+++ src/main/java/jp/co/dogtype/controller/DogController.java
@@ -0,0 +1,14 @@
+package jp.co.dogtype.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DogController {
+
+	@RequestMapping("/list")
+	public String list() {
+		return "list";
+	}
+
+}
