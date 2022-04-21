4b72df763ca0c276be77ea3812505b61e5154dd0
yo-chan <whitesnow.wave@gmail.com>
2022-04-21 16:37:49 +0900

    画面表示を修正

--- src/main/java/jp/co/dogtype/controller/DogController.java
+++ src/main/java/jp/co/dogtype/controller/DogController.java
@@ -1,13 +1,20 @@
 package jp.co.dogtype.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jp.co.dogtype.entity.Dog;
 
 @Controller
 public class DogController {
 
 	@RequestMapping("/list")
 	public String list() {
+		List<Dog> result = new ArrayList<Dog>();
+
 		return "list";
 	}
 
---
+++ src/main/java/jp/co/dogtype/entity/Dog.java
@@ -0,0 +1,48 @@
+package jp.co.dogtype.entity;
+
+public class Dog {
+
+	// DogタイトルNo
+	private int titleNo;
+	// 犬種
+	private String breed;
+	// 出身国
+	private String country;
+	// 特徴
+	private String characteristic;
+
+	public Dog(int titleNo, String breed, String country, String characteristic) {
+	}
+
+	public int getTitleNo() {
+		return titleNo;
+	}
+
+	public void setTitleNo(int titleNo) {
+		this.titleNo = titleNo;
+	}
+
+	public String getBreed() {
+		return breed;
+	}
+
+	public void setBreed(String breed) {
+		this.breed = breed;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getCharacteristic() {
+		return characteristic;
+	}
+
+	public void setCharacteristic(String characteristic) {
+		this.characteristic = characteristic;
+	}
+}
