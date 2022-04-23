8ff9aefb380e24a5405e583f2562b089b629367f
yo-chan <whitesnow.wave@gmail.com>
2022-04-23 21:52:25 +0900

    listを画面に表示

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into dog(title_no, breed, country, characteristic) values (1, 'チワワ', 'メキシコ', '世界一小さい');
+insert into dog(title_no, breed, country, characteristic) values (2, 'トイプードル', 'フランス', '日本で一番人気がある');
+insert into dog(title_no, breed, country, characteristic) values (3, 'ダックスフンド', 'ドイツ', '胴が長い');
